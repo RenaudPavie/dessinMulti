e26bb4b62d6191445354aec415d151dbf2e5e9db
Unknown <renaud.hpavie@gmail.com>
2017-11-07 13:49:27 +0100

    ajout du projet dessins multijoueurs

---
+++ index4.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Dessin multijoueur</title>
+</head>
+<body>
+	<input type="color" id="colorPicker">
+	<input type="text" id="sizeT">
+
+	<canvas id="myCanvas" width="1000" height="650" style="border: solid black 2px"></canvas>
+
+	
+<script
+  src="https://code.jquery.com/jquery-3.2.1.min.js"
+  integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="
+  crossorigin="anonymous"></script>
+
+<script src="js/index4.js"></script>
+
+</body>
+</html>
---
+++ js/index4.js
@@ -0,0 +1,52 @@
+var path = [];
+var myMouse = false;
+
+var dernierX = 0;
+var dernierY = 0;
+
+function drawPath(){
+		var ctx = $('#myCanvas')[0].getContext('2d');
+		ctx.moveTo(dernierX,dernierY);
+		ctx.lineTo(event.offsetX,event.offsetY);
+		ctx.strokeStyle = document.getElementById('colorPicker').value;
+		ctx.lineWidth = document.getElementById('sizeT').value;
+		ctx.stroke();
+		dernierX = event.offsetX;
+		dernierY = event.offsetY;
+
+}
+
+$('#myCanvas')
+	.mousemove(function(){
+		if (myMouse === true) {
+			path.push([event.offsetX, event.offsetY]);
+			drawPath()
+		}
+	})
+	.mousedown(function(){
+		path = [];
+		myMouse = true;
+		dernierX = event.offsetX;
+		dernierY = event.offsetY;
+	})
+	.mouseup(function(){
+		myMouse=false;
+			var data = {
+				path: path,
+				strokeColor : document.getElementById('colorPicker').value,
+				lineWidth : document.getElementById('sizeT').value
+			}
+
+			var query = {
+				url : 'http://draw.api.niamor.com/paths/add',
+				method: 'POST',
+				data: data
+				
+			}
+		$.ajax(query);	
+	})
+
+
+
+
+		
